cb5b3c5277a0a028b1074ee20eb3af1cb4bd23ef
Dmitriy Shestavin <dimatwl@gmail.com>
2012-07-25 10:53:20 +0400

    Test servlet.

--- src/icloude/CloudServlet.java
+++ src/icloude/CloudServlet.java
@@ -11,17 +11,36 @@ public class CloudServlet extends HttpServlet {
 	
 	private final Database db = new Database();
 	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello from GET");
 		
-		for (int i = 0; i < 10; i++) {
-			db.add(new TestData("" + i));
+		if (req.getParameterMap().containsKey("testWord")) {
+			String testWord = req.getParameter("testWord");
+			resp.getWriter().println("Parameter 'testWord' recieved.");
+			if (db.contains(testWord)) {
+				resp.getWriter().println("Word '" + testWord + "' found in database.");
+			} else {
+				resp.getWriter().println("Word '" + testWord + "' not found in database.");
+			}
+		} else {
+			resp.getWriter().println("There is no parameter 'testWord'.");
 		}
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello from POST");
 		
-		for (int i = 0; i < 10; i++) {
-			resp.getWriter().println(db.contains((i * 2) + ""));
+		if (req.getParameterMap().containsKey("testWord")) {
+			String testWord = req.getParameter("testWord");
+			resp.getWriter().println("Parameter 'testWord' recieved.");
+			db.add(new TestData(testWord));
+			resp.getWriter().println("Word '" + testWord + "' added to database.");
+		} else {
+			resp.getWriter().println("There is no parameter 'testWord'.");
 		}
 	}
 }
