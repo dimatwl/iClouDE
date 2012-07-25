d06828d18b7abe9733ccc22cfc6170713647a165
Dmitriy Shestavin <dimatwl@gmail.com>
2012-07-25 15:51:28 +0400

    JSON

--- src/icloude/CloudServlet.java
+++ src/icloude/CloudServlet.java
@@ -1,21 +1,40 @@
 package icloude;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import storage.Database;
 import storage.TestData;
+
+import com.google.gson.Gson;
 
 @SuppressWarnings("serial")
 public class CloudServlet extends HttpServlet {
 	
 	private final Database db = new Database();
 	
+	private class SimpleMessage {
+		private String text;
+		
+		public SimpleMessage (String inpWord) {
+			text = inpWord;
+		}
+	}
+	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello from GET");
+		resp.setContentType("text/JSON");
+		//resp.getWriter().println("Hello from GET");
 		
+		SimpleMessage msg = new SimpleMessage("Hello from GET.");
+		Gson gson = new Gson();
+		String json = gson.toJson(msg);
+		
+		resp.getWriter().println(json);
+		/*
 		if (req.getParameterMap().containsKey("testWord")) {
 			String testWord = req.getParameter("testWord");
 			resp.getWriter().println("Parameter 'testWord' recieved.");
@@ -27,20 +46,29 @@ public class CloudServlet extends HttpServlet {
 		} else {
 			resp.getWriter().println("There is no parameter 'testWord'.");
 		}
+		*/
 	}
 	
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello from POST");
+		resp.setContentType("text/JSON");
+		//resp.getWriter().println("Hello from POST");
 		
-		if (req.getParameterMap().containsKey("testWord")) {
-			String testWord = req.getParameter("testWord");
+		SimpleMessage msg = new SimpleMessage("Hello from POST.");
+		Gson gson = new Gson();
+		String json = gson.toJson(msg);
+		
+		resp.getWriter().println(json);
+		
+		/*
+		if (req.getParameterMap().containsKey("testJSON")) {
+			String testWord = req.getParameter("testJSON");
 			resp.getWriter().println("Parameter 'testWord' recieved.");
 			db.add(new TestData(testWord));
 			resp.getWriter().println("Word '" + testWord + "' added to database.");
 		} else {
-			resp.getWriter().println("There is no parameter 'testWord'.");
+			resp.getWriter().println("There is no parameter 'testJSON'.");
 		}
+		*/
 	}
 }
