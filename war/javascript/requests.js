63f59ec17f7d39c82d6a6576147c6161316df0e7
sweetybanana <iloveska>
2012-07-26 19:08:10 +0400

    added messaging with json and response view

--- war/javascript/requests.js
+++ war/javascript/requests.js
@@ -14,17 +14,26 @@ function get(message) {
 }
 
 function post(message) {
-    //alert(message);
+    var jsonMsg = $.toJSON({data: message});
+    
 	$.ajax({
         //url: "http://spbau-icloude.appspot.com/rest/api/info",
         url: "http://localhost:8888/rest/api/info",
         type: "POST",
         dataType: "json",
-        data: {data: message},
-        success: function(data) {alert("good post!" + "\n" + data.text);},
+        data: {json: jsonMsg},
+        success: function(data) {handler(data);}, 
         error: function(a) {alert("bad post!");},
     });
 }
+
+function handler(data) {
+	alert(data.text);
+	$('#status').text(data.text);
+}
+
+
+
 
 
 function repeat() {
@@ -34,13 +43,23 @@ function repeat() {
 
 
 $(document).ready(function() {
-    $('#getBtn').click(get);
+    
+	$('#getBtn').click(get);
     $('#postBtn').click(function() {
     	post('hey yo');
     });
+    
     $('#repeatBtn').click(repeat);
+    
     $('#sendBtn').click(function() {
     	alert($('#codeArea').val());
     	post($('#codeArea').val()); 
     });
+    
+    /*
+    setInterval(function() {
+        post($('#codeArea').val());	
+    }, interval);
+    */
+    
 });
