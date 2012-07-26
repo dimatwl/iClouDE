d33a9cc17e5b9d53196bde394eb2e81a046dda9b
sweetybanana <iloveska>
2012-07-26 16:10:53 +0400

    pages stuff added

---
+++ war/javascript/requests.js
@@ -0,0 +1,30 @@
+const interval = 1000;
+
+
+function get(message) {
+    $.ajax({
+        //url: "http://spbau-icloude.appspot.com/rest/api/info",
+        url: "http://localhost:8888/rest/api/info",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {alert("good!" + "\n" + data.text);},
+        error: function(a) {alert("bad!");},
+    });
+}
+
+function post(message) {
+    $.ajax({
+        //url: "http://spbau-icloude.appspot.com/rest/api/info",
+        url: "http://localhost:8888/rest/api/info",
+        type: "POST",
+        dataType: "json",
+        data: "ep! i'm back!",
+        success: function(data) {alert("good post!" + "\n" + data.text);},
+        error: function(a) {alert("bad post!");},
+    });
+}
+
+
+function repeat() {
+    setInterval(get, interval);
+}
