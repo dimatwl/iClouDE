b7cca5346c468274bd185b9b1d7a191989e6f574
Dmitriy Shestavin <dimatwl@gmail.com>
2012-07-27 14:59:37 +0400

    'New file' stub done.

---
+++ src/icloude/request_handlers/NewFileRequestHandler.java
@@ -0,0 +1,48 @@
+package icloude.request_handlers;
+
+
+import icloude.requests.NewFileRequest;
+import icloude.responses.StandartResponse;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * @author DimaTWL
+ * Handling all requests on "rest/newfile"
+ * URL: rest/newfile
+ * Method: POST
+ * Required response: Standart
+ */
+@Path("/newfile")
+public class NewFileRequestHandler {
+	private Gson gson = new Gson();
+	
+	/**
+	 * This method used to handle all POST request on "rest/newfile" 
+	 * @return the StandartResponse witch will be sent to client.
+	 */
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public String postInfoJSON(@FormParam("json") String inpJSON) {
+		StandartResponse responce;
+		if (inpJSON == null) {
+			responce = new StandartResponse("Error", false, "No 'json' parameter in http request");
+		} else {
+			try {
+				NewFileRequest fromJSON = gson.fromJson(inpJSON, NewFileRequest.class);
+				responce = new StandartResponse(fromJSON.getRequestID(), true, "Request for new file creation recieved.");
+			} catch (JsonSyntaxException e) {
+				responce = new StandartResponse("Error", false, "Bad JSON syntax.");
+			}
+		}
+		return gson.toJson(responce);
+	}
+}
