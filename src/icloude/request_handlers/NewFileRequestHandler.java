6ca7c87fde9f99fac87e60e2b6e4c17d2fee0015
Dmitriy Shestavin <dimatwl@gmail.com>
2012-07-27 15:11:28 +0400

    Fix for new file stub.

--- src/icloude/request_handlers/NewFileRequestHandler.java
+++ src/icloude/request_handlers/NewFileRequestHandler.java
@@ -26,15 +26,15 @@ public class NewFileRequestHandler {
 	private Gson gson = new Gson();
 	
 	/**
-	 * This method used to handle all POST request on "rest/newfile" 
-	 * @return the StandartResponse witch will be sent to client.
+	 * This method used to handle all POST request on "rest/newfile"
+	 * @return the StandartResponse witch will be sent to client
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public String postInfoJSON(@FormParam("json") String inpJSON) {
+	public String post(@FormParam("json") String inpJSON) {
 		StandartResponse responce;
 		if (inpJSON == null) {
-			responce = new StandartResponse("Error", false, "No 'json' parameter in http request");
+			responce = new StandartResponse("Error", false, "No 'json' parameter in http request.");
 		} else {
 			try {
 				NewFileRequest fromJSON = gson.fromJson(inpJSON, NewFileRequest.class);
@@ -44,5 +44,15 @@ public class NewFileRequestHandler {
 			}
 		}
 		return gson.toJson(responce);
+	}
+	
+	/**
+	 * This method used to handle all POST request on "rest/newfile" 
+	 * @return error message
+	 */
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String get() {
+		return new String("GET method is not allowed here.");
 	}
 }
