00ef7f923dfbf8f5c2335d1a8acdecbcc0ad9a39
Dmitriy Shestavin <dimatwl@gmail.com>
2012-07-30 15:31:50 +0400

    "Download file" stub.

--- src/icloude/request_handlers/UploadFileRequestHandler.java
+++ src/icloude/request_handlers/UploadFileRequestHandler.java
@@ -33,14 +33,14 @@ public class UploadFileRequestHandler {
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public String post(@FormParam("json") String inpJSON) {
+	public String post(@FormParam("json") String json) {
 		StandartResponse responce;
-		if (inpJSON == null) {
+		if (json == null) {
 			responce = new StandartResponse("Error", false,
 					"No 'json' parameter in http request.");
 		} else {
 			try {
-				UploadFileRequest fromJSON = gson.fromJson(inpJSON,
+				UploadFileRequest fromJSON = gson.fromJson(json,
 						UploadFileRequest.class);
 				if (fromJSON.getRequestType().equals("uploadfile")) {
 					responce = new StandartResponse(fromJSON.getRequestID(),
--- src/icloude/responses/StandartResponse.java
+++ src/icloude/responses/StandartResponse.java
@@ -1,7 +1,8 @@
 package icloude.responses;
 
 /**
- * @author DimaTWL This class describes 'Standart' response from protocol.
+ * @author DimaTWL 
+ * This class describes 'Standart' response from protocol.
  */
 public class StandartResponse extends BaseResponse {
 
