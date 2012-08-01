bb09f1ec22188f8fae830134143ea58441804c46
Dmitriy Shestavin <dimatwl@gmail.com>
2012-08-01 14:38:16 +0400

    For testing.

--- src/icloude/contents/FileContent.java
+++ src/icloude/contents/FileContent.java
@@ -3,39 +3,44 @@ package icloude.contents;
 import java.util.Date;
 
 /**
- * @author DimaTWL This class represents "FileContent" entity from protocol
+ * @author DimaTWL
+ * This class represents "FileContent" entity from protocol
  */
 public class FileContent {
 	private String type;
 	private String fileID;
 	private String text;
 	private String fileType;
+	private String ownerID;
 	private String revisionID;
-	private String creationDate; //TODO smth
-	private String modificationDate; //TODO smth
-
+	private Date creationDate;
+	private Date modificationDate;
+	
 	/**
 	 * @param type
 	 * @param fileID
 	 * @param text
 	 * @param fileType
+	 * @param ownerID
 	 * @param revisionID
 	 * @param creationDate
 	 * @param modificationDate
 	 */
 	public FileContent(String type, String fileID, String text,
-			String fileType, String revisionID, String creationDate,
-			String modificationDate) {
+			String fileType, String ownerID, String revisionID,
+			Date creationDate, Date modificationDate) {
+		super();
 		this.type = type;
 		this.fileID = fileID;
 		this.text = text;
 		this.fileType = fileType;
+		this.ownerID = ownerID;
 		this.revisionID = revisionID;
 		this.creationDate = creationDate;
 		this.modificationDate = modificationDate;
 	}
-
-	private FileContent() {
+	
+	private FileContent(){
 	}
 
 	/**
@@ -67,6 +72,13 @@ public class FileContent {
 	}
 
 	/**
+	 * @return the ownerID
+	 */
+	public String getOwnerID() {
+		return ownerID;
+	}
+
+	/**
 	 * @return the revisionID
 	 */
 	public String getRevisionID() {
@@ -76,14 +88,14 @@ public class FileContent {
 	/**
 	 * @return the creationDate
 	 */
-	public String getCreationDate() {
+	public Date getCreationDate() {
 		return creationDate;
 	}
 
 	/**
 	 * @return the modificationDate
 	 */
-	public String getModificationDate() {
+	public Date getModificationDate() {
 		return modificationDate;
 	}
 
