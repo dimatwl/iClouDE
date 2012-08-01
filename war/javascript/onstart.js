632d7e171c91eab09b1ca727d8341cfb23450b93
sweetybanana <iloveska>
2012-08-01 19:14:37 +0400

    syntax light added

--- war/javascript/onstart.js
+++ war/javascript/onstart.js
@@ -3,13 +3,23 @@ var projectID;
 
 var currentFileID;
 
-
+//var editor = CodeMirror.fromTextArea(document.getElementById("#codeArea"));
 
 
 
 $(document).ready(function() {
     
-
+	var editor = CodeMirror.fromTextArea(document.getElementById("codeArea"), {
+		  mode: "text/x-java",
+		  lineNumbers: true,
+		  lineWrapping: true,
+		  onCursorActivity: function() {
+		    editor.setLineClass(hlLine, null, null);
+		    hlLine = editor.setLineClass(editor.getCursor().line, null, "activeline");
+		  }
+		});
+    
+	var hlLine = editor.setLineClass(0, "activeline");
 	
 	
 	$('#CreateNewFileBtn').click(function() {
