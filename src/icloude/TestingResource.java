f030c1f87fd731ee49588e4eaf700fc0fd00b650
Dmitriy Shestavin <dimatwl@gmail.com>
2012-08-02 14:32:19 +0400

    no message

---
+++ src/icloude/TestingResource.java
@@ -0,0 +1,23 @@
+package icloude;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import storage.DatabaseException;
+
+@Path("/testing")
+public class TestingResource {
+
+//	@GET
+//	@Produces(MediaType.TEXT_HTML)
+//	public String getInfoJSON() throws IOException, DatabaseException {	
+//		StringBuilder outputMessageBuilder = new StringBuilder();
+//		outputMessageBuilder
+//		return outputMessage
+//	}
+
+}
