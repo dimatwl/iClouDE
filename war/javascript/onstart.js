70b0a25feb4a026fa84ea4dc407bc239c7e93c8e
sweetybanana <iloveska>
2012-08-13 13:20:30 +0400

    getting all projects from start and making tree :0

--- war/javascript/onstart.js
+++ war/javascript/onstart.js
@@ -1,44 +1,23 @@
 $(document).ready(function() {
-	
-	
-	Protocol.downloadProjectsList.request.setResponseHandler(function(data) {
-		arr = [];
-		for (obj in data.content) {
-		    arr[obj.name] = obj.value;
-		}
-		
-		var str = "";
-		for (i in arr)
-			str += i.projectName;
-		
-	    alert(arr.length);	
-				
-	    /*
-		$('#tree').jstree({
+    Protocol.downloadProjectsList.request.setResponseHandler(function(data) {
+	    arr = [];
+	    for (i in data.content) {
+	        var tmp = data.content[i];
+	    	obj = {
+	    	    data: tmp.projectName,
+	    	    attr: {type: 'file', projectID: tmp.projectID}	    	    		
+	    	} 	
+	    	arr.push(obj);       
+	    }		
+	    
+	    $('#tree').jstree({
 		    plugins: ["themes", "json_data", "ui", "crrm", "hotkeys", "contextmenu"],
-		    json_data: arr,
+		    json_data: {data: arr},
 		    contextmenu: {
 		        items: customMenu    
 		    }
 		});
-		*/		
-	
 	});
-	
-	
+    
 	Protocol.downloadProjectsList.request.send(userID);
-	
-	
-	/*
-	
-	$('#tree').jstree({
-	    plugins: ["themes", "json_data", "ui", "crrm", "hotkeys", "contextmenu"],
-	    json_data: data;
-	    contextmenu: {
-	        items: customMenu    
-	    }
-	});
-	
-	*/
-	
 });
